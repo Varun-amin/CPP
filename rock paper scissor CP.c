891d3edbe34fa7b3df8e1db7e4f49e5d424a1671
Varun Amin <87900166+Varun-amin@users.noreply.github.com>
2022-12-01 00:11:44 +0530

    Update rock paper scissor CP.c

--- rock paper scissor CP.c
+++ rock paper scissor CP.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//main
+//main lojic
 int rockPaperScissor (char you, char comp)
 {
   if (you != 'r' && you != 'p' && you != 's' && you != 'q')
